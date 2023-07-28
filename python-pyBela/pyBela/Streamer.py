680f236d35656b87db3595796fe49447958ec36d
Teresa Pelinski <teresapelinski@gmail.com>
2023-07-28 18:21:22 +0100

    fixed streamer saving method

--- python-pyBela/pyBela/Streamer.py
+++ python-pyBela/pyBela/Streamer.py
@@ -1,11 +1,11 @@
 import asyncio
 import array
-import pickle
-import traceback
-import os
+import aiofiles  # async file i/o
+import json
+import copy
 from collections import deque  # circular buffers
 from .Watcher import Watcher
-import copy 
+
 
 class Streamer(Watcher):
     def __init__(self, ip="192.168.7.2", port=5555, data_add="gui_data", control_add="gui_control"):
@@ -24,13 +24,15 @@ class Streamer(Watcher):
         self._streaming_buffer = None
         self._streaming_mode = None  # OFF, FOREVER, N_FRAMES :: this flag prevents writing into the streaming buffer unless requested by the user using the start/stop_streaming() functions
         self._streaming_buffer_available = asyncio.Event()
-        
+
         self._saving_enabled = False
         self._saving_filename = None
         self._saving_task = None
         self._active_saving_tasks = []
-        self._saved_var1 = 0
-        self._saved_var2 = 0
+
+        self._file_locks = {}
+
+    # setters & getters
 
     @property
     def streaming_buffer_size(self):
@@ -52,76 +54,130 @@ class Streamer(Watcher):
         self._streaming_buffer = {var: deque(
             maxlen=self._streaming_buffer_size) for var in self.watcher_vars}
 
-    def start_streaming(self, variables=[], saving_enabled=False, saving_filename=None):  # stream forever until stopped
+    # --- public methods
+
+    # stream forever until stopped
+    def start_streaming(self, variables=[], saving_enabled=False, saving_filename=None):
+        """_summary_
+
+        Args:
+            variables (list, optional): List of variables to be streamed. Defaults to [].
+            saving_enabled (bool, optional): Enables/disables saving streamed data to local file. Defaults to False.
+            saving_filename (_type_, optional) Filename for saving the streamed data. Defaults to None.
+        """
         self._saving_enabled = True if saving_enabled else False
         self._saving_filename = "streamed_data.pkl" if saving_filename is None and saving_enabled else saving_filename
         # if no variables are specified, stream all watched variables
-        if len(variables)==0:
+        if len(variables) == 0:
             variables = self.watcher_vars
         variables = variables if isinstance(variables, list) else [variables]
-            
+
         self._streaming_mode = "FOREVER"
         self.start()
         self.send_ctrl_msg(
             {"watcher": [{"cmd": "watch", "watchers": variables}]})
 
+    def stop_streaming(self, variables=[]):
+        """_summary_
+
+        Args:
+            variables (list, optional): List of variables to stop streaming. Defaults to [].
+
+        Returns:
+            streaming_buffer (dict): Dict containing the streaming buffers for each streamed variable.
+        """
+        return asyncio.run(self.async_stop_streaming(variables))
+
     async def async_stop_streaming(self, variables=[]):
         self.stop()
         self._streaming_mode = "OFF"
-        
+
         if self._saving_enabled:
             self._saving_enabled = False
             self._saving_filename = None
-            await asyncio.gather(*self._active_saving_tasks, return_exceptions=True) # await last self._saving_task
-            #self._saving_task.cancel()  
-            self._active_saving_tasks.clear()
-        
-        if variables==[]:
-            variables = self.watched_vars # stop streaming all watcher variables
+            await asyncio.gather(*self._active_saving_tasks, return_exceptions=True)
+            self._active_saving_tasks.clear()  # await all saving tasks
+
+        if variables == []:
+            variables = self.watched_vars  # stop streaming all watcher variables
         self.send_ctrl_msg(
             {"watcher": [{"cmd": "unwatch", "watchers": variables}]})
-        
+
         return self.streaming_buffer
 
-    def stop_streaming(self, variables=[]):
-        return asyncio.run(self.async_stop_streaming(variables))
-
-
     async def async_stream_n_frames(self, variables, n_frames, saving_enabled=False, saving_filename=None):
+        # resizes the streaming buffer size to n_frames and returns it when full
         self._saving_enabled = True if saving_enabled else False
         self._saving_filename = "streamed_data.pkl" if saving_filename is None and saving_enabled else saving_filename
 
         # if no variables are specified, stream all watched variables
-        if len(variables)==0:
+        if len(variables) == 0:
             variables = self.watcher_vars
         variables = variables if isinstance(variables, list) else [variables]
-        
+
         self.start()
         self._streaming_mode = "N_FRAMES"  # flag cleared in __rec_msg_callback
         # using setter to automatically resize buffer
         self.streaming_buffer_size = n_frames
-     
-        self.send_ctrl_msg({"cmd":"unwatch", "watchers":self.watched_vars}) # unwatch all variables
-        self.send_ctrl_msg( # watch only the variables specified
-            {"watcher": [{"cmd": "watch", "watchers": variables}]})
+
+        # unwatch all variables
+        self.send_ctrl_msg({"cmd": "unwatch", "watchers": self.watched_vars})
+        self.send_ctrl_msg(
+            {"watcher": [{"cmd": "watch", "watchers": variables}]})  # watch only the variables specified
 
         # return when finished filling buffer
         await self._streaming_buffer_available.wait()
         self._streaming_buffer_available.clear()
 
-        self.stop_streaming(variables) # turns off listener, unwatches variables
+        # turns off listener, unwatches variables
+        self.stop_streaming(variables)
 
         return self.streaming_buffer
 
-    # resizes the streaming buffer size to n_frames and returns it when full 
-    def stream_n_frames(self, variables=[], n_frames=1000, delay=0, saving_enabled=False, filename=None): # blocks a cell because it waits for the result. in order not to block the cell, use the async version above
+    def stream_n_frames(self, variables=[], n_frames=1000, delay=0, saving_enabled=False, filename=None):
+        """_summary_
+
+        Args:
+            variables (list, optional): List of variables to be streamed. Defaults to [].
+            n_frames (int, optional): Number of frames to stream. Defaults to 1000.
+            delay (int, optional): _description_. Defaults to 0.
+            saving_enabled (bool, optional): Enables/disables saving streamed data to local file. Defaults to False.
+            saving_filename (_type_, optional) Filename for saving the streamed data. Defaults to None.
+
+        Returns:
+            streaming_buffer (dict): Dict containing the streaming buffers for each streamed variable.
+        """
         # TODO implement delay once data comes timestamped
         return asyncio.run(self.async_stream_n_frames(variables, n_frames, saving_enabled, filename))
-    
+
     def is_streaming(self):
         return True if self._streaming_mode != "OFF" else False
 
-        
+    def load_data_from_file(self, filename):
+        try:
+            data = []
+            with open(filename, "r") as f:  # Open the file in binary read mode asynchronously
+                while True:
+                    line = f.readline()
+                    if not line:
+                        break
+                    try:
+                        # Load the next object from the file
+
+                        data_list = json.loads(line)
+
+                        data.extend(data_list)
+                    except EOFError:
+                        # Reached the end of the file
+                        break
+        except Exception as e:
+            print(f"Error while loading data from file: {e}")
+            return None
+
+        return data
+
+    # --- private methods
+
     def _parse_data_message(self, msg):
         global _type, _channel
         if self._streaming_mode != "OFF":
@@ -131,61 +187,36 @@ class Streamer(Watcher):
             elif len(msg) > 3:
                 _msg = array.array(_type, msg).tolist()
                 self._streaming_buffer[self._watcher_vars[_channel]].extend(
-                    _msg)  #FIXME this does not truncate once buffer size is reached 
+                    _msg)  # FIXME this does not truncate once buffer size is reached
                 if self._saving_enabled:
-                    _save_var = copy.copy(_msg) # avoid racing conditions
-                    if _channel == 0:
-                        self._saved_var1 += len(_msg)
-                    if _channel==1:
-                        self._saved_var2 += len(_msg)
+                    # avoid racing conditions # FIXME do everything in binary here?
+                    _save_msg = copy.copy(_msg)
                     # Save the data asynchronously
-                    saving_task = asyncio.create_task(self._save_data_to_file(f"{self._watcher_vars[_channel]}_{self._saving_filename}", _save_var))
-                    saving_task.add_done_callback(lambda task: self._active_saving_tasks.remove(task))
-                    self._active_saving_tasks.append(saving_task) 
+                    saving_task = asyncio.create_task(self._save_data_to_file(
+                        f"{self._watcher_vars[_channel]}_{self._saving_filename}", _save_msg))
+                    self._active_saving_tasks.append(saving_task)
 
                 if self._streaming_mode == "N_FRAMES" and all(len(self._streaming_buffer[var]) == self._streaming_buffer_size for var in self._watcher_vars):
                     self._streaming_mode = "OFF"
                     self._streaming_buffer_available.set()
-                    
-    async def _save_data_to_file(self, filename, data_var):
+
+    async def _save_data_to_file(self, filename, msg):
         # TODO warning if file already exists
         try:
             while self._saving_enabled:
-                data_to_save = copy.copy(data_var)
-                with open(filename, "ab") as f:  # Open the file in binary append mode
-                    pickle.dump(data_to_save, f, protocol=pickle.HIGHEST_PROTOCOL)
-                    # Wait for a short interval before writing the next update
-                    await asyncio.sleep(0.1)
+                _msg = copy.copy(msg)
+
+                if filename not in self._file_locks.keys():  # make sure there are not two processes writing to the same file
+                    # create lock for file if it does not exist
+                    self._file_locks[filename] = asyncio.Lock()
+                async with self._file_locks[filename]:
+
+                    # Open the file in binary append mode
+                    async with aiofiles.open(filename, "a") as f:
+                        _json = json.dumps(_msg)
+                        await f.write(_json+"\n")
+                        # Wait for a short interval before writing the next update
+                # ensures data is not duplicated -- allows time for another message to arrive
+                await asyncio.sleep(1)
         except Exception as e:
             print(f"Error while saving data to file: {e}")
-            traceback.print_exc()
-            
-    
-    # TODO test -- start saving but streaming is running already 
-    def save_data_as_it_comes(self, filename):
-        self._saving_enabled = True
-        self._saving_filename = filename
-
-    def stop_saving_data(self):
-        self._saving_enabled = False
-        self._saving_filename = None
-        
-    def load_data_from_file(self, filename):
-        try:
-            data = []
-            with open(filename, "rb") as f:  # Open the file in binary read mode
-                while True:
-                    try:
-                        # Load the next object from the file
-                        data_list = pickle.load(f)
-
-                        data.append(data_list)
-                    except EOFError:
-                        # Reached the end of the file
-                        break
-
-                return data
-        except Exception as e:
-            print(f"Error while loading data from file: {e}")
-            return None
-
--- python-pyBela/test.py
+++ python-pyBela/test.py
@@ -3,8 +3,9 @@ import asyncio
 import os
 from pyBela import Watcher, Streamer
 
+
 class test_Watcher(unittest.TestCase):
-    
+
     print("test_Watcher...")
 
     def test_list(self):
@@ -13,7 +14,7 @@ class test_Watcher(unittest.TestCase):
         watcher.start()
         self.assertEqual(len(watcher.list()), len(watcher.watcher_vars),
                          "Length of list should be equal to number of watcher variables")
-        
+
     def test_start_stop(self):
         watcher = Watcher()
         watcher.start()
@@ -22,66 +23,81 @@ class test_Watcher(unittest.TestCase):
                          "Watcher ctrl listener should be None after stop")
         self.assertEqual(watcher._data_listener, None,
                          "Watcher data listener should be None after stop")
-          
+
 
 class test_Streamer(unittest.TestCase):
-    
+
     print("test_Streamer...")
-    
+
     async def async_stream_forever_modes(self):
         streamer = Streamer()
         streamer.start_streaming(variables=streamer.watcher_vars)
-        self.assertEqual(streamer._streaming_mode, "FOREVER","Streaming mode should be FOREVER after start_streaming")
+        self.assertEqual(streamer._streaming_mode, "FOREVER",
+                         "Streaming mode should be FOREVER after start_streaming")
         await asyncio.sleep(0.5)
         streamer.stop_streaming(variables=streamer.watcher_vars)
-        self.assertEqual(streamer._streaming_mode, "OFF","Streaming mode should be OFF after stop_streaming")
-        
+        self.assertEqual(streamer._streaming_mode, "OFF",
+                         "Streaming mode should be OFF after stop_streaming")
+
     def test_stream_forever_modes(self):
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         loop.run_until_complete(self.async_stream_forever_modes())
-        
-        
+
     def test_stream_n_frames(self):
         streamer = Streamer()
-        streaming_buffer = streamer.stream_n_frames(variables=streamer.watcher_vars, n_frames=100)
-        self.assertTrue(all(len(var_buffer) == streamer.streaming_buffer_size for var_buffer in streaming_buffer.values()))
-        
-    async def async_streamed_variables(self): 
+        streaming_buffer = streamer.stream_n_frames(
+            variables=streamer.watcher_vars, n_frames=100)
+        self.assertTrue(all(len(var_buffer) == streamer.streaming_buffer_size for var_buffer in streaming_buffer.values(
+        )), "All streamed variables should have the same length")
+
+    async def async_streamed_variables(self):
         streamer = Streamer()
-        streamer.start_streaming(variables=streamer.watcher_vars[-1]) # stream only the last variable
+        # stream only the last variable
+        streamer.start_streaming(variables=streamer.watcher_vars[-1])
         await asyncio.sleep(0.5)
         streamer.stop_streaming(variables=streamer.watcher_vars[-1])
-        self.assertNotEqual(len(streamer.streaming_buffer[streamer.watcher_vars[-1]]),0, "The streamed variable buffer should not be empty")
-        
+        self.assertNotEqual(len(
+            streamer.streaming_buffer[streamer.watcher_vars[-1]]), 0, "The streamed variable buffer should not be empty")
+
     def test_streamed_variables(self):
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         loop.run_until_complete(self.async_streamed_variables())
-        
+
     async def async_save(self):
         streamer = Streamer()
         streamer.streaming_buffer_size = 10000
-        streamer.start_streaming(variables=streamer.watcher_vars, saving_enabled=True, saving_filename="test_save.pkl")
-        os.remove("myvar_test_save.pkl")
-        os.remove("myvar2_test_save.pkl")
-        await asyncio.sleep(3 )
-        streamer.stop_streaming(variables=streamer.watcher_vars)
-        await asyncio.sleep(10)
-        loaded_1 = streamer.load_data_from_file("myvar_test_save.pkl")
-        loaded_2 = streamer.load_data_from_file("myvar2_test_save.pkl")
+        saving_filename = "test_save.txt"
 
-        print(len(loaded_1),len(loaded_2), streamer._saved_var1, streamer._saved_var2)
-        print(len(streamer.streaming_buffer["myvar"]), len(streamer.streaming_buffer["myvar2"]))
-        self.assertTrue(os.path.exists("myvar_test_save.pkl"), "The saved file should exist")
-    
+        for var in streamer.watcher_vars:
+            if os.path.exists(f"{var}_test_save.txt"):
+                os.remove(f"{var}_test_save.txt")
+
+        streamer.start_streaming(variables=streamer.watcher_vars,
+                                 saving_enabled=True, saving_filename=saving_filename)
+        await asyncio.sleep(0.5)
+        streamer.stop_streaming(variables=streamer.watcher_vars)
+
+        loaded = {}
+        for var in streamer.watcher_vars:
+            loaded[var] = streamer.load_data_from_file(f"{var}_test_save.txt")
+
+        self.assertTrue(all(len(streamer.streaming_buffer[var]) == len(loaded[var]) for var in streamer.watcher_vars),
+                        "The loaded data should have the same length as the streamed data (considering the buffer size is large enough)")
+        
+        for var in streamer.watcher_vars:
+            if os.path.exists(f"{var}_test_save.txt"):
+                os.remove(f"{var}_test_save.txt")
+
     def test_save(self):
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         loop.run_until_complete(self.async_save())
-        
+
     # TODO add test for starting streaming and starting save after a while
-            
+
+
 if __name__ == '__main__':
     # begin the unittest.main()
     unittest.main()
