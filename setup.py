7a9185cf9907938df1f3c70111340dc7d5938221
Teresa Pelinski <teresapelinski@gmail.com>
2023-10-25 16:13:57 +0100

    added packages explicitly in setup.py, added build to dev dependencies. tested build

--- setup.py
+++ setup.py
@@ -1,22 +1,8 @@
 import setuptools
-import os
-import json
-
-
-# Load the dependencies from Pipfile.lock
-pipfile_lock_path = os.path.join(os.path.dirname(__file__), 'Pipfile.lock')
-with open(pipfile_lock_path, 'r') as lockfile:
-    pipfile_lock_data = json.load(lockfile)
-
-# Extract the dependencies from 'default' section
-install_requires = []
-if 'default' in pipfile_lock_data.get('dependencies', {}):
-    install_requires = list(pipfile_lock_data['dependencies']['default'])
-
 
 setuptools.setup(
     name="pyBela",
-    version="0.1.0",  # Update with your package version
+    version="0.0.0",  # Update with your package version
     author="Teresa Pelinski",
     author_email="teresapelinski@gmail.com",
     description="pyBela",
@@ -24,7 +10,18 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/BelaPlatform/pyBela",
     packages=setuptools.find_packages(),
-    install_requires=install_requires,
+    install_requires=[
+        "jupyter",
+        "panel",
+        "jupyter-bokeh",
+        "bitarray",
+        "notebook",
+        "websockets",
+        "bokeh",
+        "ipykernel",
+        "nest-asyncio",
+        "aiofiles",
+        "paramiko"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
