8ebdd4e2b244cf41f60610ec51c29fa6aea12111
pelinski <teresapelinski@gmail.com>
2025-02-04 20:29:12 +0000

    example projects only as links

--- example-projects/on-device-inference/bela-drumsynth/src/AppOptions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-struct AppOptions
-{
-    std::string modelPath = "";
-    std::string audioFile = "";
-};
--- example-projects/on-device-inference/container-scripts/download-torch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-mkdir -p /sysroot/opt/pytorch-install
-
-url=https://github.com/pelinski/bela-torch/releases/download/v2.5.1/pytorch-v2.5.1.tar.gz
-echo "Downloading Pytorch from $url"
-wget -O - $url | tar -xz -C /sysroot/opt/pytorch-install
